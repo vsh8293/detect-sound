4e8f4b5d1fab01a52770f6f7dffdffb9f3844eb7
vsh8293 <78829270+vsh8293@users.noreply.github.com>
2021-02-19 10:39:12 +1300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+music.setBuiltInSpeakerEnabled(true)
+basic.forever(function () {
+    if (input.soundLevel() >= 70) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+    }
+})
